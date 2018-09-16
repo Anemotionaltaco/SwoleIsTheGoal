8a8da48033535bb3dfadabcbb02f95bd751bcfff
anemotionaltaco <essencejordan56@yahoo.com>
2018-09-15 18:06:24 -0700

    xcode stuff

---
+++ SwolePatrol/SwolePatrol/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  SwolePatrol
+//
+//  Created by Essence Jordan on 9/15/18.
+//  Copyright © 2018 Essence Jordan. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
